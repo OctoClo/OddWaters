724d30cac7dbe1cd0004715117e5acdc74724814
Claudia <octoclo@zoho.com>
2019-03-18 18:24:13 +0100

    Move objects with mouse

---
+++ OddWaters/Assets/_Project/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    Camera mainCamera;
+    Interactible grabbedObject;
+    Vector3 grabbedOjectScreenPos;
+    Vector3 grabbedObjectOffset;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (grabbedObject)
+            {
+                grabbedObject = null;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Interactible interactible = hit.collider.GetComponent<Interactible>();
+                if (interactible)
+                {
+                    grabbedObject = interactible;
+                    grabbedOjectScreenPos = mainCamera.WorldToScreenPoint(grabbedObject.gameObject.transform.position);
+                    grabbedObjectOffset = grabbedObject.gameObject.transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, grabbedOjectScreenPos.z));
+                }
+            }
+        }
+
+        if (grabbedObject)
+        {
+            Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, grabbedOjectScreenPos.z);
+            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos) + grabbedObjectOffset;
+            grabbedObject.gameObject.transform.position = mouseWorldPos;
+        }
+    }
+}
