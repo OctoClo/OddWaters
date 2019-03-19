ec93ef548aada5eff63bbb950f7680a783fe9892
Claudia <octoclo@zoho.com>
2019-03-19 15:36:07 +0100

    Desk borders

--- OddWaters/Assets/_Project/Scripts/InputManager.cs
+++ OddWaters/Assets/_Project/Scripts/InputManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject desk;
+    float deskMinX;
+    float deskMaxX;
+    float deskMinZ;
+    float deskMaxZ;
+
     Camera mainCamera;
     Interactible grabbedObject;
     Vector3 grabbedOjectScreenPos;
@@ -11,6 +18,13 @@ public class InputManager : MonoBehaviour
 
     void Start()
     {
+        Vector3 deskPosition = desk.transform.position;
+        Vector3 deskScale = desk.transform.localScale;
+        deskMinX = deskPosition.x - deskScale.x / 2;
+        deskMaxX = deskPosition.x + deskScale.x / 2;
+        deskMinZ = deskPosition.z - deskScale.z / 2;
+        deskMaxZ = deskPosition.z + deskScale.z / 2;
+
         mainCamera = Camera.main;
     }
 
@@ -26,24 +40,33 @@ public class InputManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            GrabObject();
+        }        
+    }
+
+    void GrabObject()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 20.0f))
+        {
+            Interactible interactible = hit.collider.GetComponent<Interactible>();
+            if (interactible)
             {
-                Interactible interactible = hit.collider.GetComponent<Interactible>();
-                if (interactible)
-                {
-                    grabbedObject = interactible;
-                    grabbedOjectScreenPos = mainCamera.WorldToScreenPoint(grabbedObject.gameObject.transform.position);
-                    grabbedObjectOffset = grabbedObject.gameObject.transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, grabbedOjectScreenPos.z));
-                }
+                grabbedObject = interactible;
+                grabbedOjectScreenPos = mainCamera.WorldToScreenPoint(grabbedObject.gameObject.transform.position);
+                grabbedObjectOffset = grabbedObject.gameObject.transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, grabbedOjectScreenPos.z));
             }
         }
+    }
 
+    void FixedUpdate()
+    {
         if (grabbedObject)
         {
             Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, grabbedOjectScreenPos.z);
             Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos) + grabbedObjectOffset;
-            grabbedObject.gameObject.transform.position = mouseWorldPos;
+            if (mouseWorldPos.x >= deskMinX && mouseWorldPos.x <= deskMaxX && mouseWorldPos.z >= deskMinZ && mouseWorldPos.z <= deskMaxZ)
+                grabbedObject.MoveTo(mouseWorldPos);
         }
     }
 }
--- OddWaters/Assets/_Project/Scripts/Interactible.cs
+++ OddWaters/Assets/_Project/Scripts/Interactible.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Interactible : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     public virtual void Test()
     {
 
+    }
+
+    public void MoveTo(Vector3 newPosition)
+    {
+        rigidBody.MovePosition(newPosition);
     }
 }
