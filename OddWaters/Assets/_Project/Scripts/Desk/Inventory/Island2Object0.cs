b38750295b22e9bbd9d0f6d402f196502e312677
Clément <clement.pilate@gmail.com>
2019-05-24 12:40:01 +0200

    Integrate Stone object (Island2Object0)

---
+++ OddWaters/Assets/_Project/Scripts/Desk/Inventory/Island2Object0.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island2Object0 : MonoBehaviour
+{
+
+    Color inactiveColor = Color.black;
+    Color activeColor = new Color(57,191, 61);
+
+    Color currentColor;
+
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentColor = Color.Lerp(inactiveColor, activeColor, Mathf.PingPong(Time.time, 1));
+
+        mat.SetColor("_EmissionColor", currentColor / 20.0f);
+    }
+}
