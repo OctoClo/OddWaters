7fddf0cae9f6854dece3add0b944f271baf16710
Clément <clement.pilate@gmail.com>
2019-05-31 11:35:49 +0200

    Startup menu + Pause menu prefab

---
+++ OddWaters/Assets/_Project/Scripts/UI/Menu/Options.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject Controls;
+
+    [SerializeField]
+    Animator ENAnimator;
+    [SerializeField]
+    Animator FRAnimator;
+
+    private void Start()
+    {
+
+        //Get current language, select button
+        ENAnimator.SetTrigger("Activate");
+    }
+
+    public void OnClickControls()
+    {
+        Controls.SetActive(true);
+    }
+
+    public void OnClickControlsBack()
+    {
+        Controls.SetActive(false);
+    }
+
+    public void OnClickLanguageButton(string language)
+    {
+        SetLanguage(language);
+    }
+
+    void SetLanguage(string language)
+    {
+
+    }
+}
